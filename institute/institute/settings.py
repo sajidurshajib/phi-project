e0ffc92b04242504adb7a37e673e0bcbdaa86d42
sajidurshajib <sajidur.inbox@gmail.com>
2024-09-29 02:56:02 +0600

    Revert "change: nav"
    This reverts commit 52bd1df85cc50871f29151e1753ecd1a3c5ba8c8.

--- institute/institute/settings.py
+++ institute/institute/settings.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
     'base.apps.BaseConfig',
     'accounts.apps.AccountsConfig',
     'dashboard.apps.DashboardConfig',
-    'management.apps.ManagementConfig',
     'events.apps.EventsConfig',
 ]
 
